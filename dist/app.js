823023d620433c615541581d01d3c8fcf1374e62
Anurag Chaturvedi <anuragChaturvedi978@gmail.com>
2023-06-23 11:13:33 +0530

    feat: implement burn event listen

---
+++ dist/app.js
@@ -0,0 +1,31 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ethers = require("ethers");
+require("dotenv").config();
+function getTransfer() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const contractAddress = "0x35C66732340c4b8cb283f82fF539C1b0c1a97E36"; ///USDC Contract
+        const provider = new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/9b39dd722354497dbfbbed837ae61831");
+        const ABI = [{ "inputs": [{ "internalType": "address", "name": "_efiToken", "type": "address" }, { "internalType": "address", "name": "_enjToken", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "user", "type": "address" }, { "indexed": true, "internalType": "address", "name": "token", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "TokenBurned", "type": "event" }, { "inputs": [], "name": "MAX_TOKENS_TO_BURN", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_token", "type": "address" }, { "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "burn", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "efiToken", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "enjToken", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "totalBurnedAmount", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
+        const contract = new ethers.Contract(contractAddress, ABI, provider);
+        contract.on("TokenBurned", (user, token, amount, event) => {
+            let transferEvent = {
+                user: user,
+                token: token,
+                amount: amount,
+                eventData: event,
+            };
+            console.log(JSON.stringify(transferEvent, null, 4));
+        });
+    });
+}
+getTransfer();
